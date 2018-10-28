7505d3e22ac1c62aea88293655fab91d5b07d09b
jlee167 <jlee167@illinois.edu>
2018-10-28 16:06:31 +0000

    Encryption routine fixed: Preserving image format.  Decryption needs working on type conversion

--- Python/imgdata.py
+++ Python/imgdata.py
@@ -1,9 +1,19 @@
 import numpy as np
+import copy
 
-class img_data:
+
+class ImgProcessor:
+
+    # RGB data array
     R = []
     G = []
     B = []
+
+    # RGB
+    SecretData_R = []
+    SecretData_G = []
+    SecretData_B = []
+
     def __init__(self, row, column):
         self.row_size = row
         self.column_size = column
@@ -11,6 +21,8 @@ class img_data:
 
     def capture(self,image):
         self.img = image
+        self.secret_img = copy.copy(image)
+        self.RGB_sort()
 
     def RGB_sort(self):
         count = -1
@@ -31,10 +43,15 @@ class img_data:
         self.B = np.fft.fft2(self.B)
 
     def recover(self):
-        self.R = np.real(np.fft.ifft2(self.R))
-        self.G = np.real(np.fft.ifft2(self.G))
-        self.B = np.real(np.fft.ifft2(self.B))
-
+        self.R_int_result = np.fft.ifft2(self.R)
+        self.G_int_result = np.fft.ifft2(self.G)
+        self.B_int_result = np.fft.ifft2(self.B)
+        self.R_real = np.real(self.R_int_result)
+        self.G_real = np.real(self.G_int_result)
+        self.B_real = np.real(self.B_int_result)
+        self.R_imag = np.imag(self.R_int_result)
+        self.G_imag = np.imag(self.G_int_result)
+        self.B_imag = np.imag(self.B_int_result)
 
     def copy(self):
         row_counter = -1
@@ -45,6 +62,9 @@ class img_data:
                 pixel[0] = self.R[row_counter][counter]
                 pixel[1] = self.G[row_counter][counter]
                 pixel[2] = self.B[row_counter][counter]
+                self.secret_img[row_counter][counter][0] = self.R_imag[row_counter][counter]
+                self.secret_img[row_counter][counter][1] = self.G_imag[row_counter][counter]
+                self.secret_img[row_counter][counter][2] = self.B_imag[row_counter][counter]
                 counter = counter + 1
 
     def phase_mask(self, key):
@@ -53,6 +73,7 @@ class img_data:
                 self.R[x][y] *= key[x][y]
                 self.G[x][y] *= key[x][y]
                 self.B[x][y] *= key[x][y]
+
     def phase_unmask(self, key):
         for x in range (0, self.row_size):
             for y in range (0, self.column_size):
@@ -60,7 +81,7 @@ class img_data:
                 self.G[x][y] /= key[x][y]
                 self.B[x][y] /= key[x][y]
 
-    def print(self):
-        print(self.R)
-        print(self.G)
-        print(self.B)
+    def showPixelData(self):
+        print("R Data\n" + self.R)
+        print("G Data \n" + self.G)
+        print("B Data \n" + self.B)
